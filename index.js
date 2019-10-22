4996a01e904b516bdb5d444e829be28b0a2d755f
Ani Hopkins <ani.d.hopkins@gmail.com>
2019-10-21 23:23:50 -0400

    Added tilemaps, got sidescrolling to work.

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+const WORLD = {
+  WIDTH: 1600,
+  HEIGHT: 600
+};
+
 const config = {
   type: Phaser.AUTO,
   width: 800,
@@ -17,11 +22,13 @@ const config = {
 
 // Things to do before game loads
 function preload () {
+  // Load tilemap for the background
+  this.load.tilemapTiledJSON('map', 'assets/tilemaps/background.json');
+  this.load.spritesheet('skyline', 'assets/tilemaps/tileset.png', {frameWidth: 60, frameHeight: 60});
 
   // Load image assets
   this.load.image('player', 'assets/player.png');
-  this.load.image('floor', 'assets/floor.png');
-  this.load.image('skyline', 'assets/skyline.png');
+  // this.load.image('floor', 'assets/floor.png');
 
   // Create a cursor object to track key presses
   cursors = this.input.keyboard.createCursorKeys();
@@ -29,22 +36,29 @@ function preload () {
 
 // Things that happen when the game starts
 function create() {
-  // Add an image for the game background
-  this.add.image(800, 300, 'skyline');
-  // Create a static physics group and add the floor to it
-  floor = this.physics.add.staticGroup();
-  floor.create(800, 575, 'floor');
+  // Create tilemap
+  map = this.make.tilemap({key: 'map'});
+  let backgroundTiles = map.addTilesetImage('tiles', 'skyline');
+
+  // Load tiles and set up collisions with the ground.
+  skylineLayer = map.createDynamicLayer('background', backgroundTiles, 0 , 0);
+  groundLayer = map.createDynamicLayer('floor', backgroundTiles, 0 , 0);
+  groundLayer.setCollisionByExclusion([-1]);
 
   // Add player sprite and give it physics properties
-  player = this.physics.add.image(100, 400, 'player');
-  //player.setCollideWorldBounds(true);
+  player = this.physics.add.image(100, 300, 'player');
 
   // Make the player and floor collide
-  this.physics.add.collider(player, floor);
+  this.physics.add.collider(groundLayer, player);
+
+  // Set world's "edges"
+  this.physics.world.bounds.width = WORLD.WIDTH;
+  this.physics.world.bounds.height = WORLD.HEIGHT;
+  player.setCollideWorldBounds(true);
 
   // Lock the camera inside the bounds of the game world and make it track the
   // player
-  this.cameras.main.setBounds(0, 0, 1600, 600);
+  this.cameras.main.setBounds(0, 0, WORLD.WIDTH, WORLD.HEIGHT);
   this.cameras.main.startFollow(player);
 }
 
@@ -60,15 +74,9 @@ function update() {
     player.setVelocityX(0)
   }
 
-  console.log
   // Vertical jump when up arrow is pushed
   if (cursors.up.isDown && player.body.onFloor()) {
     player.setVelocityY(-500);
-  }
-
-  // TODO: Remove this. For debugging only.
-  if (cursors.down.isDown) {
-    console.log("Player X: " + player.x);
   }
 }
 
